06b16006440424db76a1ae02b4bc93fd58cc720b
Sanjay <sanjaykeloth321@gmail.com>
2025-02-16 17:02:15 +0530

    chore(git): remove dist from tracking and update .gitignore

--- dist/src/users/entities/user.entity.d.ts
+++
@@ -1,8 +0,0 @@
-import { Roles } from 'src/utility/common/user-roles.enum';
-export declare class UserEntity {
-    id: number;
-    name: string;
-    email: string;
-    password: string;
-    roles: Roles;
-}
--- dist/src/utility/common/user-roles.enum.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Roles = void 0;
-var Roles;
-(function (Roles) {
-    Roles["ADMIN"] = "admin";
-    Roles["USER"] = "user";
-})(Roles || (exports.Roles = Roles = {}));
-//# sourceMappingURL=user-roles.enum.js.map
